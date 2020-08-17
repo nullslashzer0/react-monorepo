6a9743487606d0486624633c82149e8f8f6e35fb
Robert Wadsworth <robert.wadsworth@marketingevolution.com>
2020-08-17 10:16:08 -0700

    updated repo

--- src/applications/client1/src/App.js
+++ src/applications/client1/src/App.js
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const test = {
+    foo: "hello world"
+  };
+  console.log(test);
   return (
     <div className="App">
       <header className="App-header">
